57ba94e8f593ce593dc730423964a562b7fb2297
oli9787 <olialagan75@gmail.com>
2021-11-21 08:51:03 +0530

    text

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,29 @@
+package org.add;
+
+public class GreensTech {
+	private void greensOmr() {
+		System.out.println("omr is to long");
+	}
+	private void greensAdayar() {
+		System.out.println("A2B");
+	}
+	private void greensTambaram() {
+		System.out.println("im staying in tambaram");
+	}
+	private void GreensVelachery() {
+		System.out.println("im not studing velachery branch");
+	}
+	private void greensAnnaNagar() {
+		System.out.println("golden flat");
+	}
+	public static void main(String[] args) {
+		GreensTech gt = new GreensTech();
+		gt.greensOmr();
+		gt.greensAdayar();
+		gt.greensTambaram();
+		gt.GreensVelachery();
+		gt.greensAnnaNagar();
+		
+	}
+
+}
